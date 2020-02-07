33dd99b1462d3f0842a4f4fa9af8a2d4fa67f019
araceliCumen <aycuevam@gamil.com>
2020-02-07 01:58:25 -0500

    integrando react reduc

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import './App.css';
 import { createStore } from 'redux';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import everis from './reducers'
+
 import 'bootstrap/dist/css/bootstrap.css';
 
 import List from './componentes/pages/List';
+import Login from './componentes/pages/Login';
+
+
+let store = createStore(everis);
 
 function App() {
   return (
-    <div className="App">
-      <List/>
-    </div>
+
+    <Router>
+      <div>
+        <Route exact path="/login/" render={(props) => <PasoInicio {...props} store={store} />} />
+        <Route path="/cuentas/pasoSeguridad" render={(props) => <PasoSeguridad {...props} store={store} />} />
+        <Route path="/cuentas/pasoEleccionCuenta" render={(props) => <PasoEleccionCuenta {...props} store={store} />} />
+        <Route path="/cuentas/pasoIdentidad" render={(props) => <PasoIdentidad {...props} store={store} />} />
+      </div>
+    </Router>
   );
 }
 
