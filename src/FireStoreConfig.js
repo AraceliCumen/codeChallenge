da087fd360ced2d46089cdf0bed54b0a89b5758b
araceliCumen <aycuevam@gamil.com>
2020-02-06 23:57:17 -0500

    agregando firebase

---
+++ src/FireStoreConfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+
+import 'firebase/firestore';
+
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCnQrP0TIudEFM1UvBtvrWg5f-KErcEuHM",
+    authDomain: "retoeveriscodechallenge.firebaseapp.com",
+    databaseURL: "https://retoeveriscodechallenge.firebaseio.com",
+    projectId: "retoeveriscodechallenge",
+    storageBucket: "retoeveriscodechallenge.appspot.com",
+    messagingSenderId: "448709001308",
+    appId: "1:448709001308:web:b0b60d19da9fea3fbb6699",
+    measurementId: "G-FZ71HF58VK"
+});
+
+
+let db = firebase.firestore();
+db.settings({timestampsInSnapshots:true});
+export default db;
