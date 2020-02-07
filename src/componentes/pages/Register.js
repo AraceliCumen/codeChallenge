d09cbdbe5c06a70efb32b5bd2ea46dbccd85a8a9
araceliCumen <aycuevam@gamil.com>
2020-02-07 03:02:17 -0500

    mejorando registrar

--- src/componentes/pages/Register.js
+++ src/componentes/pages/Register.js
@@ -2,16 +2,17 @@ import React, {Component} from 'react';
 // import '../../css/Register.css';
 
 import * as utils from '../utils';
+import db from '../../FireStoreConfig';
 
 class Register extends Component {
 
     state ={
-        username: '',
+        userName: '',
         password: '',
         firstName : '',
         lastName: '',
         email: '',
-        estado: '',
+        estado: 1,
     }
 
     onChangeUsername = e => {
@@ -44,14 +45,18 @@ class Register extends Component {
         })
     }
 
-    onChangeEstado = e => {
-        this.setState({
-            estado: e.target.value,
-        })
+    onButtonClickListar = e => {
+        this.props.history.replace('/everis/list');
     }
 
     onButtonClick = e => {
-        const {}
+        db.collection('users').add({
+            user:this.state,
+        }).then(()=>{
+            alert('agregado');
+        }).catch(()=>{
+            alert('error al agregar');
+        })
     }
 
 
@@ -59,31 +64,32 @@ class Register extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-md-offset-5 col-md-3">
+                    <div className="col-md-offset-1 col-md-5">
                         <div className="form-login">
                             <h4>Registrar</h4>
                             <br />
                             <p>Nombre de Usuario:</p>
-                            <input type="text" id="userNameRegister" className="form-control input-sm chat-input" placeholder="username" />
+                            <input type="text" id="userNameRegister" onChange={this.onChangeUsername} className="form-control input-sm chat-input" placeholder="username" />
                             <br />
                             <p>Password:</p>
-                            <input type="text" id="userPasswordRegister" className="form-control input-sm chat-input" placeholder="password" />
+                            <input type="text" id="userPasswordRegister" onChange={this.onChangePassword} className="form-control input-sm chat-input" placeholder="password"  type="password"/>
                             <br />
                             <p>Nombres:</p>
-                            <input type="text" id="firstName" className="form-control input-sm chat-input" placeholder="firstName" />
+                            <input type="text" id="firstName" onChange={this.onChangeFirstName} className="form-control input-sm chat-input" placeholder="firstName" />
                             <br />
                             <p>Apellidos:</p>
-                            <input type="text" id="lastName" className="form-control input-sm chat-input" placeholder="lastName" />
+                            <input type="text" id="lastName" onChange={this.onChangeLastName} className="form-control input-sm chat-input" placeholder="lastName" />
                             <br />
                             <p>Email:</p>
-                            <input type="text" id="email" className="form-control input-sm chat-input" placeholder="email" />
-                            <br />
-                            <p>Estado:</p>
-                            <input type="text" id="estado" className="form-control input-sm chat-input" placeholder="estado" />
+                            <input type="text" id="email" onChange={this.onChangeEmail} className="form-control input-sm chat-input" placeholder="email" />
                             <br />
                             <div className="wrapper">
                                 <span className="group-btn">
                                 <button className="btn btn-primary btn-md margin10" onClick={this.onButtonClick}>Registrar</button>
+                                </span>
+
+                                <span className="group-btn">
+                                <button className="btn btn-primary btn-md margin10" onClick={this.onButtonClickListar}>Listar</button>
                                 </span>
                             </div>
                         </div>
